7af6b9663ec15974f4de42ac20004fdd97d312fe
SDET Automation <58454416+muraribec@users.noreply.github.com>
2024-04-10 06:12:28 +0530

    Update TestRunner.java
    dhdfhdh

--- src/test/java/com/qa/facebook/automation/testrunners/TestRunner.java
+++ src/test/java/com/qa/facebook/automation/testrunners/TestRunner.java
@@ -14,6 +14,7 @@ import io.cucumber.junit.CucumberOptions;
 		plugin = {"pretty","json:target/json-report/cucumber.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 	//testing git repooioriyory
+	     System.out.println("Hello");
 		)
 
 public class TestRunner {
